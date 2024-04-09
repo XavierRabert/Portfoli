63202d2f2777ec40e9cc5f6378ab4d6c5d4b1ad2
Xavier <xrabert@gmail.com>
2024-04-09 14:01:34 +0200

    personal portfoli

---
+++ src/types/projects.ts
@@ -0,0 +1,8 @@
+export type projectDetails = {
+  imgSrc: string;
+  imgName: string;
+  webLink: string;
+  text: string;
+  textLink?: string;
+  showProject: boolean;
+};
---
+++ tailwind.config.mjs
@@ -0,0 +1,20 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        "x-dark-blue": "#222831",
+        "x-dark-grey": "#31363F",
+        "x-blue": "#76ABAE",
+        "x-grey": "#EEEEEE",
+      },
+    },
+  },
+  plugins: [],
+};
